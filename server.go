a04408652307c5f5ce02de02fedfbb920ad0e65a
Lucas S. Silva <57202598+LNaoEdeLixo@users.noreply.github.com>
2021-01-18 20:03:53 -0300

    First Experiment

---
+++ server.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./pages"))
+	http.Handle("/", fs)
+
+	log.Println("Servidor funcionando! Porta :5000...")
+	err := http.ListenAndServe(":5000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
